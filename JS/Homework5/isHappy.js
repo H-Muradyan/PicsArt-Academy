39ff8f863891a5f57273c31ccdf4812edbf4c14c
H-Muradyan <hrach.muradyan.1996@gmail.com>
2022-07-23 20:03:35 +0400

    Homework 5 - JS

--- JS/Homework5/isHappy.js
+++ JS/Homework5/isHappy.js
@@ -1,14 +1,14 @@
-function isHappy (number) {
-    let sum = 1;
-    for(let num of String(number)) {
-        sum *= Math.pow(num, 2)
-    }
-    return String(sum).length >= 4 ? false : true
+function isHappy(number) {
+  let sum = 1;
+  for (let num of String(number)) {
+    sum *= Math.pow(num, 2);
+  }
+  return String(sum).length >= 4 ? false : true;
 }
 
-console.log(isHappy(67))
-console.log(isHappy(89))
-console.log(isHappy(139))
-console.log(isHappy(1327))
-console.log(isHappy(2871))
-console.log(isHappy(3970))
+console.log(isHappy(67));
+console.log(isHappy(89));
+console.log(isHappy(139));
+console.log(isHappy(1327));
+console.log(isHappy(2871));
+console.log(isHappy(3970));
